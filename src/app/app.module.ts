f9e11288a8c877df5233682393c6c6a15dae35e3
augustobrigo <profeaugustobriga@gmail.com>
2024-03-06 10:24:56 +0100

    comunicacionHuerto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HuertoComponent } from './huerto/huerto.component';
+import { HortalizaComponent } from './hortaliza/hortaliza.component';
+import { FrutalComponent } from './frutal/frutal.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HuertoComponent,
+    HortalizaComponent,
+    FrutalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
